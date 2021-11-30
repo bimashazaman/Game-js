a7d35f79a4af445e3660c3d1a4a08d0e845771b9
bimashazaman <bimashazaman1234@gmail.com>
2021-11-30 19:05:01 +0600

    initial commit

--- js/gameManager.js
+++ js/gameManager.js
@@ -6,13 +6,19 @@ let gameManager = {
   resetPlayer: function (classType) {
     switch (classType) {
       case 'Warrior':
-        player = new Player('Warrior', 100, 100, 10, 10, 10)
+        player = new Player(classType, 100, 100, 10, 10, 10)
         break
       case 'Mage':
-        player = new Player('Mage', 100, 100, 10, 10, 10)
+        player = new Player(classType, 100, 100, 10, 10, 10)
         break
       case 'Rogue':
-        player = new Player('Rogue', 100, 100, 10, 10, 10)
+        player = new Player(classType, 100, 100, 10, 10, 10)
+        break
+      case 'Hunter':
+        player = new Player(classType, 100, 100, 10, 10, 10)
+        break
+      case 'Priest':
+        player = new Player(classType, 100, 100, 10, 10, 10)
         break
       default:
         console.log('Error: No class selected')
@@ -39,6 +45,8 @@ let gameManager = {
                 <h2>${player.speed}</h2>
             </div>
         </div>
+        
+
     `
   },
   setPreFight: function () {},
